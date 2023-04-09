53e7bdb103d5addf1c83872765bb2853fb553416
YoZaiKK <memocargo@hotmail.com>
2023-04-08 19:24:27 -0600

    something

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,7 +1,7 @@
-export default function About () {
+export default function AboutPage () {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Esto es el about</h1>
       {/* <Link to="/about">About</Link> */}
     </div>
   )
